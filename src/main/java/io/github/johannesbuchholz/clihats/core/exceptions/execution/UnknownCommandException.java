2aa92222a51ab6c10390820b56ba654389ab4c9f
jbuchholz <johannesbuchholz@outlook.com>
2023-03-18 18:40:26 +0100

    fix(core): Remove UnknownCommandException error message indent

--- src/main/java/io/github/johannesbuchholz/clihats/core/exceptions/execution/UnknownCommandException.java
+++ src/main/java/io/github/johannesbuchholz/clihats/core/exceptions/execution/UnknownCommandException.java
@@ -1,12 +1,11 @@
 package io.github.johannesbuchholz.clihats.core.exceptions.execution;
 
 import io.github.johannesbuchholz.clihats.core.execution.Commander;
-import io.github.johannesbuchholz.clihats.util.TextUtils;
 
 public class UnknownCommandException extends CommanderExecutionException {
 
     private static String generateErrorMessage(String[] inputArgs) {
-        return String.format("Could not find command for input arguments %s", TextUtils.indentEveryLine(String.join(" ", inputArgs)));
+        return String.format("Could not find command for input arguments %s", String.join(" ", inputArgs));
     }
 
     public UnknownCommandException(Commander failingCommander, String[] inputArgs) {
