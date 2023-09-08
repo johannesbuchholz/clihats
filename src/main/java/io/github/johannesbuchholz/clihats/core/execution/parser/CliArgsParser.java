d067a8757c403c8e87e773950a967a749f63535c
jbuchholz <johannesbuchholz@outlook.com>
2023-09-09 00:24:42 +0200

    refactor: Simplify input argument parsing method

--- src/main/java/io/github/johannesbuchholz/clihats/core/execution/parser/CliArgsParser.java
+++ src/main/java/io/github/johannesbuchholz/clihats/core/execution/parser/CliArgsParser.java
@@ -7,18 +7,18 @@ import java.util.stream.Collectors;
 
 public class CliArgsParser implements ArgsParser {
 
-    private final List<ParserToken<AbstractArgumentParser<?>>> optionParsers;
-    private final List<ParserToken<AbstractArgumentParser<?>>> operandParsers;
+    private final List<ParserToken<AbstractOptionParser<?>>> optionParsers;
+    private final List<ParserToken<AbstractOperandParser<?>>> operandParsers;
 
     public CliArgsParser(List<AbstractArgumentParser<?>> abstractParsers) {
-        List<ParserToken<AbstractArgumentParser<?>>> optionParsers = new ArrayList<>();
-        List<ParserToken<AbstractArgumentParser<?>>> operandParsers = new ArrayList<>();
+        List<ParserToken<AbstractOptionParser<?>>> optionParsers = new ArrayList<>();
+        List<ParserToken<AbstractOperandParser<?>>> operandParsers = new ArrayList<>();
         int targetPosition = 0;
         for (AbstractArgumentParser<?> abstractParser : abstractParsers) {
             if (abstractParser instanceof AbstractOptionParser) {
-                optionParsers.add(new ParserToken<>(targetPosition, abstractParser));
+                optionParsers.add(new ParserToken<>(targetPosition, (AbstractOptionParser<?>) abstractParser));
             } else if (abstractParser instanceof AbstractOperandParser) {
-                operandParsers.add(new ParserToken<>(targetPosition, abstractParser));
+                operandParsers.add(new ParserToken<>(targetPosition, (AbstractOperandParser<?>)abstractParser));
             } else {
                 throw new IllegalArgumentException("Encountered parser of unknown type " + abstractParser.getClass());
             }
@@ -33,11 +33,10 @@ public class CliArgsParser implements ArgsParser {
         Object[] parsedValues = new Object[optionParsers.size() + operandParsers.size()];
 
         // parse options
-        parseNextRound(optionParsers, args, parsedValues);
+        parseOptions(optionParsers, args, parsedValues);
         args = Arrays.stream(args).filter(Objects::nonNull).toArray(InputArgument[]::new);
-
         // parse operands
-        parseNextRound(operandParsers, args, parsedValues);
+        parseOperands(operandParsers, args, parsedValues);
 
         // mark remaining args as unknown
         List<InputArgument> unknownInputArguments = Arrays.stream(args)
@@ -55,41 +54,52 @@ public class CliArgsParser implements ArgsParser {
      * @param args Array of input arguments. May contain null entries indicating that the respective element has already been parsed.
      * @param parsedValues The current parsing result builder to put values into.
      */
-    private void parseNextRound(List<ParserToken<AbstractArgumentParser<?>>> tokens, InputArgument[] args, Object[] parsedValues) throws ArgumentParsingException {
+    private void parseOptions(Collection<ParserToken<AbstractOptionParser<?>>> tokens, InputArgument[] args, Object[] parsedValues) throws ArgumentParsingException {
         Set<ParserToken<?>> remainingTokens = new HashSet<>(tokens);
-        List<ParserToken<?>> usedParsers = new ArrayList<>(tokens.size());
-
-        for (int i = 0; i < args.length; i++) {
-            boolean noneFound = true;
-            InputArgument currentArg = args[i];
-            if (currentArg == null)
-                // skip already parsed args
-                continue;
-            for (ParserToken<?> token : remainingTokens) {
-                if (args[i] == null)
-                    // argument position has already been parsed by previous parsers
-                    break;
-                ArgumentParsingResult<?> result = token.parser.parse(args, i);
-                if (result.isPresent()) {
-                    // parser could extract a value from the current arg
-                    noneFound = false;
-                    parsedValues[token.targetPosition] = result.getValue();
-                    usedParsers.add(token);
-                }
-            }
-            usedParsers.forEach(remainingTokens::remove);
-            if (noneFound && currentArg.isBreakSequence()) {
-                // current arg is unknown to the parsers of this round.
-                // Treat not yet parsed arguments prior to the break-sequence as unknown
-                List<InputArgument> unknownInputArguments = Arrays.stream(args).limit(i).filter(Objects::nonNull).collect(Collectors.toList());
-                if (!unknownInputArguments.isEmpty())
-                    throw new UnknownArgumentException(unknownInputArguments);
+        int i = -1;
+        while (++i < args.length) {
+            Collection<ParserToken<?>> usedTokens = parseArgument(args, i, remainingTokens, parsedValues);
+            usedTokens.forEach(remainingTokens::remove);
+            if (usedTokens.isEmpty() && args[i] != null && args[i].isBreakSequence()) {
                 // remove break-sequence arg
                 args[i] = null;
                 break;
             }
         }
         putDefaultValues(remainingTokens, parsedValues);
+    }
+
+    /**
+     * @param tokens Parser tokens for this round.
+     * @param args Array of input arguments. May contain null entries indicating that the respective element has already been parsed.
+     * @param parsedValues The current parsing result builder to put values into.
+     */
+    private void parseOperands(Collection<ParserToken<AbstractOperandParser<?>>> tokens, InputArgument[] args, Object[] parsedValues) throws ArgumentParsingException {
+        Set<ParserToken<?>> remainingTokens = new HashSet<>(tokens);
+        int i = -1;
+        while (++i < args.length) {
+            parseArgument(args, i, remainingTokens, parsedValues)
+                    .forEach(remainingTokens::remove);
+        }
+        putDefaultValues(remainingTokens, parsedValues);
+    }
+
+    /**
+     * @return A collection of parsers that found a value
+     */
+    private Collection<ParserToken<?>> parseArgument(InputArgument[] args, int index, Collection<ParserToken<?>> tokens, Object[] parsedValues) throws ArgumentParsingException {
+        Iterator<ParserToken<?>> tokenIterator = tokens.iterator();
+        List<ParserToken<?>> usedParsers = new ArrayList<>(tokens.size());
+        while (args[index] != null && tokenIterator.hasNext()) {
+            ParserToken<?> token = tokenIterator.next();
+            ArgumentParsingResult<?> result = token.parser.parse(args, index);
+            if (result.isPresent()) {
+                // parser could extract a value from the current arg
+                parsedValues[token.targetPosition] = result.getValue();
+                usedParsers.add(token);
+            }
+        }
+        return usedParsers;
     }
 
     /**
