ad3f57b6abd357e9ff7eae32252180fec889c138
jbuchholz <johannesbuchholz@outlook.com>
2023-09-30 22:58:57 +0200

    doc: Reformulate to match the new posix features

--- src/test/java/io/github/johannesbuchholz/clihats/readme/examples/HelloWorldCliHats.java
+++ src/test/java/io/github/johannesbuchholz/clihats/readme/examples/HelloWorldCliHats.java
@@ -10,57 +10,37 @@ import io.github.johannesbuchholz.clihats.processor.execution.CliHats;
 /**
  * A simple "Hello, World!" program to demonstrate the capabilities of CliHats.
  */
-// tag::example-class-slim[]
-// tag::annotation-cli[]
-@CommandLineInterface
-// end::annotation-cli[]
+@CommandLineInterface //<1>
 public class HelloWorldCliHats {
-    // tag::example-class-content[]
-    // tag::example-class-main[]
 
     public static void main(String[] args) {
-        CliHats.get(HelloWorldCliHats.class).execute(args);
+        CliHats.get(HelloWorldCliHats.class).execute(args); // <2>
     }
-    // end::example-class-main[]
 
     /**
      * Simply prints "Hello, World!"
      */
-    // tag::example-class-say-hello[]
-    // tag::annotation-command[]
-    @Command
-    // end::annotation-command[]
+    @Command // <3>
     public static void sayHello() {
         System.out.println("Hello, World!");
     }
-    // end::example-class-say-hello[]
 
     /**
      * Prints a greeting to the specified name.
      * @param name the name to greet.
      * @param polite if true, additionally prints "Nice to meet you!".
      */
-    // tag::example-class-say-hello-to-person[]
-    // tag::annotation-command-2[]
     @Command
-    // end::annotation-command-2[]
     public static void sayHelloToPerson(
-            // tag::annotation-options-name[]
-            @Option(necessity = OptionNecessity.REQUIRED)
-            // end::annotation-options-name[]
+            @Option(necessity = OptionNecessity.REQUIRED) // <4>
             String name,
-            // tag::annotation-options-polite[]
-            @Option(flagValue = "true", defaultValue = "false")
-            // end::annotation-options-polite[]
+            @Option(flagValue = "true", defaultValue = "false") // <5>
             Boolean polite
     ) {
         System.out.printf("Hello, %s!\n", name);
         if (polite)
             System.out.println("Nice to meet you!");
     }
-    // end::example-class-say-hello-to-person[]
-    // end::example-class-content[]
 
 }
-// end::example-class-slim[]
 // end::example-class[]
